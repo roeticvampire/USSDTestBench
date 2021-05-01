8170a8d5fb5766aff3d97aa0d8d748cbeb05ad69
Syed Ali Haider <56455410+roeticvampire@users.noreply.github.com>
2021-05-01 23:05:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,39 @@
-
 <?php
-        #We obtain the data which is contained in the post url on our server.
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
-        $text=$_GET['USSD_STRING'];
-        $phonenumber=$_GET['MSISDN'];
-        $serviceCode=$_GET['serviceCode'];
+// Comment these lines to hide errors
+
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON Welcome to the Mobile UPI Payments\n";
+    $response .= "Please enter your PIN";
+
+} else if ($text == "123456"&&$phoneNumber=="7004359495") {
+    // Business logic for first level response
+    $response = "CON Nice we can go ahead \n";
+    $response .= "1. Account number \n";
 
 
-        $level = explode("*", $text);
-        if (isset($text)) {
-   
-                //How do we add comments to this shit hue hue hue
-        if ( $text == "" ) {
-            $response="CON Welcome to the Mobile UPI Payments\nPlease enter your PIN";
-        }
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
 
-        if(isset($level[0]) && $level[0]!="" && !isset($level[1])){
-            if($level[0]=="680123" && $phonenumber=="7004359495")
-          $response="CON Hi ".$level[0].", Let's proceed!";
-          else
-             
-          $response="END Nope ".$level[0].", You can't do this!";
-        }
-        else if(isset($level[1]) && $level[1]!="" && !isset($level[2])){
-                $response="CON Please enter you national ID number\n"; 
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
 
-        }
-        else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
-            //Save data to database
-            $data=array(
-                'phonenumber'=>$phonenumber,
-                'fullname' =>$level[0],
-                'electoral_ward' => $level[1],
-                'national_id'=>$level[2]
-                );
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
 
-            
+}else {
+    $response = "END Invalid bruh \n";
+}
 
-            $response="END Thank you ".$level[0]." for registering.\nWe will keep you updated"; 
-    }
-
-        header('Content-type: text/plain');
-        echo $response;
-
-    }
-
-?>
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
